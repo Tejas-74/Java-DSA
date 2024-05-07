6413b8067bf181349a5c27450c43aff568188e64
Tejas Jadhav <tejasnjadhav76@gmail.com>
2024-05-07 17:47:02 +0530

    basic

---
+++ Basics/java.java
@@ -0,0 +1,50 @@
+// import java.util.Scanner; 
+//  public class java{
+//     public static void main(String[] args) {
+//         System.out.println("Hello World");
+//         System.out.println("4");
+//         System.out.println(4);
+//         System.out.println(4+5);
+//         System.out.println("4+5");
+//     }
+//  } 
+ 
+ 
+/*--------------------------------------------------- */
+
+ // variables
+
+//  import java.util.Scanner;
+//  public class java {
+//     public static void main(String[] args) {
+//         int x;
+//         x = 10;
+//         System.out.println(x);
+//         System.out.println(x+10);
+//         x = x + 10;
+//         System.out.println(x);
+//     }
+//  }
+
+/*------------------------------------------------------------- */
+
+// Arithmatic operation
+
+// import java.util.Scanner;
+// public class java{
+//     public static void main(String[] args) {
+//         int x = 5;
+//         int y = 2;
+//         System.out.println(x+y);
+//         System.out.println(x-y);
+//         System.out.println(x*y);
+//         System.out.println(x/y);
+
+//         System.out.println(y+x);
+//         System.out.println(y-x);
+//         System.out.println(y*x);
+//         System.out.println(y/x);
+//     }
+// }
+
+ 
