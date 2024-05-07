bcef662623372b30c0a083f00da2b4eb5f4e350e
Tejas Jadhav <tejasnjadhav76@gmail.com>
2024-05-08 01:03:59 +0530

    on going

--- Basics/java.java
+++ Basics/java.java
@@ -47,4 +47,90 @@
 //     }
 // }
 
+/*------------------------------------------------------------- */
+
+// Arithmatic opr. on double data type
+
+// import java.util.Scanner;
+// public class java {
+//    public static void main(String[] args) {
+//     double x = 5;
+//     double y = 8;
+//     System.out.println(x+y);
+//     System.out.println(x-y);
+//     System.out.println(x*y);
+//     System.out.println(x/y);
+//    }
+// }
+
+/*-------------------------------------------------------- ----------*/
+
+// cal percentage of 5 subs
+
+// import java.util.Scanner;
+// public class java {
+//    public static void main(String[] args) {
+//     double x1 = 90; //maths
+//     double x2 = 91; //science
+//     double x3 = 92; //english
+//     double x4 = 93; //hindi
+//     double x5 = 94; // chemistry
+
+//     double y = (x1+x2+x3+x4+x5)/5;
+//     System.out.println(y);
+//    }
+// }
+
+/*--------------------------------------------------------------------- */
+
+// area of circle
+
+// import java.util.Scanner;
+// public class java {
+//     public static void main(String[] args) {
+//         double radius = 5;
+//         double pi = 3.14;
+//         double area = pi*radius*radius;
+//         System.out.println(area);
+//     }
+// }
+
+/*------------------------------------------------------------------- */
+
+// cal simple interest
+
+// import java.util.Scanner;
+// public class java {
+
+//     public static void main(String[] args) {
+//         double p = 100;
+//         double rate = 20;
+//         double time = 3;
+//         double si = (p*rate*time)/100;
+//         System.out.println(si);
+//     }
+// }
+
+/*------------------------------------------------------------------ */
+
+ // taking input from user
+
+//  import java.util.Scanner;
+//  public class java {
  
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter radius of circle: ");
+//         int radius;
+//         radius = sc.nextInt();
+
+//         double pi = 3.14;
+//         double area = pi * radius * radius;
+//         System.out.println("The area of circle is :"+area);
+//     }
+//  }
+
+/*---------------------------------------------------------------------- */
+
+ // SUM OF TWO NO. INPUT
+
