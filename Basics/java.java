c8ae09756831fd5f9cf57e482d1eb90a7d2149fa
Tejas Jadhav <tejasnjadhav76@gmail.com>
2024-05-08 16:36:06 +0530

    in process

--- Basics/java.java
+++ Basics/java.java
@@ -132,5 +132,140 @@
 
 /*---------------------------------------------------------------------- */
 
- // SUM OF TWO NO. INPUT
+ // Taking input 
+
+// import java.util.Scanner;
+// public class java {
+
+//     public static void main(String[] args) {
+//         try(Scanner sc = new Scanner(System.in))
+//     {
+//         System.out.println("Whats your name ?");
+//         String name = sc.next();
+//         System.out.println("Hello"+name+"have a good day");
+//     }
+//   }
+// }
+
+/*------------------------------------------------------------------------------ */
+
+// sum of 2 no
+
+// import java.util.Scanner;
+// public class java {
+
+//     public static void main(String[] args) {
+//         try(Scanner sc = new Scanner(System.in))
+//         {
+//             System.out.println("Enter the value of x:");
+//             int x = sc.nextInt();
+
+//             System.out.println("Enter the value of y:");
+//             int y = sc.nextInt();
+            
+//             int sum = x+y;
+//             System.out.println("Sum of two no is:"+sum);
+//         }
+//     }
+// }
+
+/*----------------------------------------------------------------------- */
+
+// take two int input a,b; a>b, find remainder when a is divided by b
+
+// import java.util.Scanner;
+// public class java {
+
+//     public static void main(String[] args) {
+//         try(Scanner sc = new Scanner(System.in))
+//         {
+//            System.out.println("Enter Dividend: ");
+//            int a = sc.nextInt();
+
+//            System.out.println("Enter Divisor: ");
+//            int b = sc.nextInt();
+
+//            int r = a%b
+//            System.out.println(r);
+//         }
+//     }
+// }
+
+/*---------------------------------------------------------------------- */
+// predict the output
+
+// public class java {
+
+//     public static void main(String[] args) {
+//         int i = 2, j = 3, k, l;
+//         double a,b;
+
+//         k = i/j*j;
+//         l = j/i*i;
+//         a = i/j*j;
+//         b = j/i*i;
+//         System.out.println(k+" "+l+" "+a+" "+b);
+//     }
+// }
+
+/*-------------------------------------------------------------------------- */
+
+// ASCI value finder
+
+// abcdefghijklmnopqrstuvwxyz
+// ABCDEFGHIJKLMNOPQRSTUVWXYZ
+// !@#$%^&*(){}[]|?/:;''"".<>,~`_-=+
+// 0123456789
+
+// import java.util.Scanner;
+// public class java {
+
+//     public static void main(String[] args) {
+//         try(Scanner sc = new Scanner(System.in))
+//         {
+//             char ch = 'a';
+//             System.out.println(ch);
+//             int x = (int)ch;
+//             System.out.println(x);
+//         }
+//     }
+// }
+
+/*------------------------------------------------------------------------------- */
+
+//  convert km to m
+
+// import java.util.Scanner;
+// public class java {
+
+//     public static void main(String[] args) {
+//         try(Scanner sc = new Scanner(System.in))
+//         {
+//             System.out.println("Enter the value: ");
+//             int km = sc.nextInt();
+
+//             double miles = km * 0.621;
+//             System.out.println(miles);
+//         }
+//     }
+// }
+
+/*------------------------------------------------------------------------------- */
+
+//   comparison bet 2 no 
+
+// import java.util.Scanner;
+// public class java {
+
+//     public static void main(String[] args) {
+//         try(Scanner sc = new Scanner(System.in))
+//         {
+//             System.out.println("Enter no; ");
+//             int n = sc.nextInt();
+//             System.out.println(n>8);
+//         }
+//     }
+// }
+
+/*------------------------------------------------------------------------------ */
 
