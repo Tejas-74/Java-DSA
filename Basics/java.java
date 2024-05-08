399d03bfbd93204588bb034c3848fd046d114466
Tejas Jadhav <tejasnjadhav76@gmail.com>
2024-05-08 23:31:48 +0530

    Basic completed

--- Basics/java.java
+++ Basics/java.java
@@ -1,4 +1,7 @@
-// import java.util.Scanner; 
+
+import java.util.*;
+
+
 //  public class java{
 //     public static void main(String[] args) {
 //         System.out.println("Hello World");
@@ -269,3 +272,178 @@
 
 /*------------------------------------------------------------------------------ */
 
+// cube of num
+
+// public class java {
+
+//     public static void main(String[] args) {
+//         int x=2;
+//         int cube = x*x*x;
+
+//         System.out.println("This is side: "+x);
+//         System.out.println("This is cube: "+cube);
+//     }
+// }
+
+/*---------------------------------------------------------------------------- */
+
+// swap two no.
+
+// public class java{
+//     public static void main(String[] args) {
+//         int num1 = 2;
+//         int num2 = 3;
+
+//         System.out.println("The first number before swap is : "+num1);
+//         System.out.println("The second number before swap is : "+num2);
+
+//         int temp;
+
+//         temp=num1;
+//         num1=num2;
+//         num2=temp;
+
+//         System.out.println("the first num after swap is :"+num1);
+//         System.out.println("the second num aftefr swap is :"+num2);
+
+//     }
+// }
+
+/*------------------------------------------------------------------------------------------- */
+
+// import java.util.Scanner;
+
+// public class java {
+
+//     public static void main(String[] args) {
+//         try(Scanner sc = new Scanner(System.in))
+//         {
+//             System.out.println("Enter first name: ");
+//             String firstName = sc.next();
+
+//             System.out.println("Enter last name:");
+//             String lastName = sc.next();
+
+//             System.out.println("Enter roll.no ");
+//             int roll_no = sc.nextInt();
+
+//             System.out.println("Enter Fielf of interest");
+//             String fieldOfInterest = sc.next();
+            
+            
+//             System.out.println("Name: "+firstName+" "+lastName);
+//             System.out.println("Roll.no: "+roll_no);
+//             System.out.println("Feild of interest: "+fieldOfInterest);
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class java {
+// public static void main(String[] args) {
+// Scanner scn = new Scanner(System.in);
+// String first_name = scn.next();
+// String last_name = scn.next();
+// int roll_number = scn.nextInt();
+// String field_of_interest = scn.next();
+// System.out.println("Name: " + first_name + " " + last_name);
+// System.out.println("Roll Number: " + roll_number);
+// System.out.println("Field of interest: " + field_of_interest);
+// }
+// }
+
+
+// NOTE:- whenenver is string in use, use string tej = sc.next()
+
+/*--------------------------------------------------------------------------------------------------------------------- */
+
+// input two different string and put them into same line 
+
+// import java.util.Scanner;
+// public class Main {
+// public static void main(String[] args) {
+// Scanner scn = new Scanner(System.in);
+// String first = scn.nextLine();
+// String last = scn.nextLine();
+// System.out.println(first + last);
+// }
+// }
+
+// import java.util.Scanner;
+// public class java {
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     String first = sc.nextInt();
+//     String last = sc.next();
+//     System.out.println(first + last);
+//    }
+    
+// }
+
+/*-------------------------------------------------------------------------------------- */
+
+// write a program to add 8 to the number x and then divide it by 3, the modulus of the quotient is taken with 5 and then multiply the resultant value by 5. display the result.
+
+
+// public class java{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int x = sc.nextInt();
+//         System.out.println((((x + 8) / 3 ) % 5) * 5);
+//     }
+// }
+
+/*---------------------------------------------------------------------------------------- */
+
+// cal the sum of digits of 3-digit num. 
+
+// public class java {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int x = sc.nextInt();
+//         int sum = 0;
+
+//         while(x>0){
+//             sum += x % 10;
+//             x /= 10;
+//         }
+//         System.out.println(sum);
+//     }
+// }
+
+/*--------------------------------------------------------------------------------------------- */
+
+// Assign values of variables 'a' and 'b' as 55 and 70 respectively and then check if:
+// i) both the conditions 'a < 50' and 'a < b' are true.
+// ii) at least one of the conditions 'a < 50' or 'a < b' is true.
+
+// public class java{
+//     public static void main(String[] args) {
+//         int a = 55;
+//         int b = 70;
+//         System.out.println(a < 50 && a < b);
+//         System.out.println(a < 50 || a < b);
+//     }
+// }
+
+/*----------------------------------------------------------------------------------------------- */
+
+// Find the total number of bits needed to be flipped to convert x to y.
+// Input: 65 80
+// Output: 2
+
+// public class java{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int x = sc.nextInt();
+//         int y = sc.nextInt();
+//         int n = x^y;
+//         int count = 0;
+//         while (n != 0) {
+//             n = n & (n - 1);
+//             count++;
+//         }
+//         System.out.println(count);
+//     }
+// }
