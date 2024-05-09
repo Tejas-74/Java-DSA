11c1da213e4e38efab655098986a6fe4b5f31c7c
Tejas Jadhav <tejasnjadhav76@gmail.com>
2024-05-09 22:46:40 +0530

    if-else

---
+++ if_else/tej.java
@@ -0,0 +1,86 @@
+// take positive integer input and tell if it is even or odd
+
+// import java.util.Scanner;
+// public class loops {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter a num: ");
+//         int num = sc.nextInt();
+
+//         if (num % 2 == 0) {
+//             System.out.println("Interger is even");
+//         }
+//         if( num % 2 != 0){
+//             System.out.println("Integer is odd");
+//         }
+//     }
+// }
+
+/*--------------------------------------------------------------------------------------- */
+
+// random leap year
+
+// import java.util.Scanner;
+// public class loops{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+        
+//         System.out.println("Enter the year: ");
+//         int y = sc.nextInt();
+
+//         if (y % 4 == 0) {
+//             System.out.println("Year is leap");
+//         } else {
+//             System.out.println("Year is not leap");
+//         }
+//     }
+// }
+
+/*----------------------------------------------------------------------------- */
+
+// take integer input and print the absolute value of that integer 
+
+// import java.util.Scanner;
+// public class loops{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the integer: ");
+//         int n = sc.nextInt();
+
+//         if (n < 0) {
+//             n = n * (-1);
+//         }
+//         System.out.println("The absoute value is: "+n);
+//     }
+// }
+
+/*-------------------------------------------------------------------------------- */
+
+// if cost price and selling price of an item is input through the keyborad, write a program to determine wheather the seller has made profit or incurred loss. also determined how much profit he made or loss he incured 
+
+// import java.util.Scanner;
+
+// public class loops{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+        
+//         System.out.println("Enter cost price: ");
+//         int cp = sc.nextInt();
+
+//         System.out.println("Enter selling price: ");
+//         int sp = sc.nextInt();
+
+//         if (cp > sp) {
+//             System.out.println("Your profit is: ");
+//             System.out.println(sp-cp);
+//         }else{
+//             System.out.println("You incurred a loss");
+//             System.out.println(cp - sp);
+//         }
+//     }
+// }
+
+/*---------------------------------------------------------------------------------------- */
+
