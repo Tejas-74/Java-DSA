5c586b7025168b91b1f70756752f7138c9913490
Tejas Jadhav <tejasnjadhav76@gmail.com>
2024-07-03 16:40:38 +0530

    up

--- Basics/java.java
+++ Basics/java.java
@@ -335,6 +335,8 @@
 //     }
 // }
 
+/*--------------------------------------------------------------------------------------------------------- */
+
 // import java.util.Scanner;
 // public class java {
 // public static void main(String[] args) {
---
+++ Loops/lups.java
@@ -0,0 +1,30 @@
+// import java.util.Scanner;
+
+// public class lups{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int num=1; num <= n; num++){
+//             System.out.println(num);
+//         }
+//     }
+// }
+
+/******************************************************** */
+
+// import java.util.Scanner;
+
+// public class lups{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int num=n; num >= 1; num--){
+//             System.out.println(num);
+//         }
+//     }
+// }
+
+/*------------------------------------------------------ */
+
