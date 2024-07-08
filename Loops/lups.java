61a6a9e073e46efe161947ab7aa09b3f7db77456
Tejas Jadhav <tejasnjadhav76@gmail.com>
2024-07-08 19:12:26 +0530

    nested loops

--- Loops/lups.java
+++ Loops/lups.java
@@ -1,8 +1,6 @@
 
-//     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int n = sc.nextInt();
-
 //         for(int num=1; num <= n; num++){
 //             System.out.println(num);
 //         }
@@ -11,15 +9,14 @@
 
 import java.util.Scanner;
 
-/******************************************************** */
-
+/**
+ * ******************************************************
+ */
 // import java.util.Scanner;
-
 // public class lups{
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int n = sc.nextInt();
-
 //         for(int num=n; num >= 1; num--){
 //             System.out.println(num);
 //         }
@@ -27,9 +24,7 @@ import java.util.Scanner;
 // }
 
 /*------------------------------------------------------ */
-
 // print the first multiple of 5 which is also a multiple of 7
-
 // public class lups{
 //     public static void main(String[] args) {
 //         int num = 1;
@@ -44,7 +39,6 @@ import java.util.Scanner;
 // }
 
 /*------------------------------------------------------------------- */
-
 //print all the value between 1-50 exept for the multiple of 3
 // public class lups{
 //     public static void main(String[] args) {
@@ -56,7 +50,6 @@ import java.util.Scanner;
 //         }
 //     }
 // }
-
 // public class lups {
 //    public static void main(String[] args) {
 //     int num=1;
@@ -72,16 +65,13 @@ import java.util.Scanner;
 // }
 
 /*------------------------------------------------------------ */
-
 //count the no. of the digit for aq given no. n
-
 // public class lups{
 //     public static void main(String[] args) {
 //         Scanner sc =new Scanner(System.in);
 //         int n = sc.nextInt();
 //         int numOfDigits = 0;
 //         int original_n = n;
-
 //         while(n>0){
 //             n=n/10;
 //             numOfDigits++;
@@ -91,7 +81,6 @@ import java.util.Scanner;
 // }
 /*-------------------------------------------------------------- */
 // find the sum of digit in a given number n
-
 // public class lups{
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
@@ -107,7 +96,6 @@ import java.util.Scanner;
 // }
 /*------------------------------------------------------------------------ */
 // reverse the digit of a number
-
 // import java.util.Scanner;
 // public class lups{
 //     public static void main(String[] args) {
@@ -123,17 +111,13 @@ import java.util.Scanner;
 // }
 
 /*------------------------------------------------------------------- */
-
 // find the sum of the following series
 //   S = 1-2+3-4 ....
-
 // public class lups {
-
 //     public static void main(String[] args) {
 //     Scanner sc = new Scanner(System.in);
 //     int n = sc.nextInt();
 //     int ans = 0;
-    
 //     for( int i = 1; i <= n; i++){
 //         if(i % 2 == 0){
 //             ans -= i;
@@ -145,15 +129,12 @@ import java.util.Scanner;
 //     }
 // }
 /*----------------------------------------------------------------- */
-
 // print the n factorial number
-
 // public class lups{
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int n = sc.nextInt();
 //         int fact = 1;
-
 //         for( int i = 1; i <= n; i++){
 //             fact = fact * i;
 //             System.out.println("Factorial of " + i +": " + fact);
@@ -162,16 +143,12 @@ import java.util.Scanner;
 // }
 
 /*-------------------------------------------------------------- */
-
 // Given 2 number a and b. find a raise to the power b
-
 // public class lups {
-
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int a = sc.nextInt();
 //         int b = sc.nextInt();
-
 //         int ans = 1;
 //         for( int i = 1; i <= b; i++){
 //             ans *= a;
@@ -181,15 +158,12 @@ import java.util.Scanner;
 // }
 
 /*--------------------------------------------------------------------- */
-
 // print rectangular pattern
-
 // public class lups{
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int r = sc.nextInt();
 //         int c = sc.nextInt();
-
 //         for(int i = 1; i <= r; i++){
 //             for(int j = 1; j <= c; j++){
 //                 System.out.print("*");
@@ -200,18 +174,15 @@ import java.util.Scanner;
 // }
 
 /*------------------------------------------------------------------- */
-
 //  ******
 //  *    *
 //  *    *
 //  ******
-
 // public class lups{
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int r = sc.nextInt();
 //         int c = sc.nextInt();
-        
 //         for(int i = 1; i <= r; i++ ){
 //            for(int j = 1; j <= c; j++){
 //             if( i == 1 || i == r || j == 1 || j == c)
@@ -225,17 +196,14 @@ import java.util.Scanner;
 //     }    
 
 /*---------------------------------------------------------------- */
-
 // *
 // **
 // ***
 // ****
-
 //  public class lups{
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int r = sc.nextInt();
-
 //         for( int i = 1; i <= r; i++){
 //             for( int j = 1; j <= i; j++){
 //                 System.out.print("*");
@@ -246,17 +214,14 @@ import java.util.Scanner;
 //  }
 
 /*--------------------------------------------------------------------- */
-
 // ****
 // ***
 // **
 // *
-
 //  public class lups{
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int r = sc.nextInt();
-
 //         for( int i = 1; i <= r; i++){
 //             for( int j = 1; j <= (r+1-i); j++){
 //                 System.out.print("*");
@@ -265,12 +230,10 @@ import java.util.Scanner;
 //         }
 //     }
 //  }          OR
-
 //  public class lups{
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int r = sc.nextInt();
-
 //         for( int i = r; i >= 1; i--){
 //             for( int j = 1; j <= i; j++){
 //                 System.out.print("*");
@@ -281,35 +244,32 @@ import java.util.Scanner;
 //  }
 
 /*---------------------------------------------------------------- */
-
-    //       *
-    //      ***
-    //     *****
-    //    *******   
-    
-    // public class lups
-    // {
-    // public static void main(String[] args) {
-    // int rows;
-    // Scanner sc = new Scanner(System.in);
-    // rows=sc.nextInt();
-    // for(int i = 1, k = 0; i <= rows; ++i, k = 0)
-    // {
-    // for(int s = 1; s <= rows-i; ++s)
-    // {
-    // System.out.print(" ");
-    // }
-    // while(k != 2*i-1)
-    // {
-    // System.out.print("*");
-    // ++k;
-    // }
-    // System.out.println("");
-    // }
-    // }
-    // }
+//       *
+//      ***
+//     *****
+//    *******   
+// public class lups
+// {
+// public static void main(String[] args) {
+// int rows;
+// Scanner sc = new Scanner(System.in);
+// rows=sc.nextInt();
+// for(int i = 1, k = 0; i <= rows; ++i, k = 0)
+// {
+// for(int s = 1; s <= rows-i; ++s)
+// {
+// System.out.print(" ");
+// }
+// while(k != 2*i-1)
+// {
+// System.out.print("*");
+// ++k;
+// }
+// System.out.println("");
+// }
+// }
+// }
 /*----------------------------------------------------------------- */
-
 // 1234567
 // 2345671
 // 3456712
@@ -317,12 +277,10 @@ import java.util.Scanner;
 // 5671234
 // 6712345
 // 7123456
-
 // public class lups{
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int r = sc.nextInt();
-
 //         for( int i = 1; i <= r; i++){
 //             for( int j = i; j <= r; j++){
 //                 System.out.print(j);
@@ -339,14 +297,11 @@ import java.util.Scanner;
 // 123456
 // 123456
 // 123456
-
 // public class lups {
-
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int r = sc.nextInt();
 //         int c = sc.nextInt();
-
 //         for(int i = 1; i <= r; i++){
 //             for(int j = 1; j <= c; j++){
 //                 System.out.print(j);
@@ -360,13 +315,11 @@ import java.util.Scanner;
 // 212121
 // 121212
 // 212121
-
 // public class lups{
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int r = sc.nextInt();
 //         int c = sc.nextInt();
-
 //         for(int i = 1; i <= r; i++){
 //             for(int j = 1; j <= c; j++){
 //                 if((i+j) % 2 == 0){
@@ -384,13 +337,10 @@ import java.util.Scanner;
 // 12
 // 123
 // 1234
-
 // public class lups {
-
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int r = sc.nextInt();
-        
 //         for(int i = 1; i<=r;i++){
 //             for(int j=1; j<=i; j++){
 //                 System.out.print(j);
@@ -400,29 +350,23 @@ import java.util.Scanner;
 //     }
 // }
 /*--------------------------------------------------------------------- */
-    //     1
-    //    121  
-    //   12321
-    //  1234321  
-
+//     1
+//    121  
+//   12321
+//  1234321  
 // public class lups {
-
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int r = sc.nextInt();
-
 //         for(int i = 1; i <= r; i++){
-
 //             //print r-i spaces
 //             for(int j = 1; j <= r-i; j++){
 //                 System.out.print(" ");
 //                 }
-     
 //                 // print 1 to i
 //                 for(int k = 1; k <= i; k++){
 //                     System.out.print(k);
 //                 }
-
 //                 // print i-1 to 1
 //                 for(int l = i-1; l >= 1; l--){
 //                     System.out.print(l);
@@ -431,3 +375,123 @@ import java.util.Scanner;
 //             }
 //         }
 // }
+
+/*-------------------------------------------------------------------- */
+//       1
+//      2  2
+//     3    3
+//    44444444 
+ 
+// public class lups {
+
+//     public static void main(String[] args) {
+//         int i, j, k, rows;
+//         Scanner sc = new Scanner(System.in);
+//         rows = sc.nextInt();
+//         for (i = rows + 1; i > 1; i--) {
+//             for (j = i; j > 2; j--) {
+//                 System.out.print(" ");
+//             }
+//             for (k = 1; k <= (rows - i + 1); k++) {
+//                 if (k == 1) {
+//                     System.out.print(rows - i + 2);
+//                 } else {
+//                     System.out.print(" ");
+//                 }
+//             }
+//             for (int m = k; m >= 1; m--) {
+//                 if (m == 1) {
+//                     System.out.print(rows - i + 2);
+//                 } else {
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println("");
+//         }
+
+//     }
+// }
+/*---------------------------------------------------------------------- */
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+
+// public class lups{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for(int row = 1; row <= n; row++){
+    
+//             // for every row, run the col
+//             for( int col = 1; col <= row; col++){
+//                 System.out.print(col + " ");
+//             }
+//             System.out.println();
+//         } 
+//     }
+// }
+
+/*----------------------------------------------------------------- */
+
+//      *
+//      **
+//      ***
+//      ****
+//      *****
+//      ****
+//      ***
+//      **
+//      *
+
+// public class lups{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int row = 0; row < 2 * n; row++){
+//             int totalColInRow = row > n ? 2 * n - row: row;
+//             for(int col = 0; col < totalColInRow; col++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+/*------------------------------------------------------------------ */
+
+        //       *
+        //      * * 
+        //     * * *
+        //    * * * * 
+        //   * * * * * 
+        //  * * * * * * 
+        //   * * * * * 
+        //    * * * *
+        //     * * *
+        //      * * 
+        //       *
+        
+// public class lups {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int  row = 0; row < 2 * n; row++){
+            
+//             int totalColInRow = row > n ? 2 * n - row: row;
+//             int noOfSpaces = n - totalColInRow;
+            
+//             for(int s = 0; s < noOfSpaces; s++){
+//                 System.out.print(" ");
+//             }
+//             for(int col = 0; col < totalColInRow; col++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//     }
+} 
