4ba97d74a37d330db42984a8ee62020f6300007f
Tejas Jadhav <tejasnjadhav76@gmail.com>
2024-07-10 17:48:26 +0530

    basic

--- Basics/java.java
+++ Basics/java.java
@@ -49,6 +49,24 @@
 
 /*------------------------------------------------------------- */
 
+// Bitwise opertor
+
+// public class java{
+//     public static void main(String[] args) {
+//         int p = 9, q = 10;
+
+//         System.out.println(p | p); //11 
+//         System.out.println(p & q); //8
+//         System.out.println(p ^ q); //3
+
+//         System.out.println(p << 1); //18
+//         System.out.println(p << 2); //36
+
+//         System.out.println(q >> 1); //5
+//         System.out.println(q >> 2); //2
+//     }
+// }
+
 // Arithmatic opr. on double data type
 
 // import java.util.Scanner;
