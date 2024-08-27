13b70f655a415abc435979bbda286d2fe82849c0
Tejas Jadhav <tejasnjadhav76@gmail.com>
2024-08-27 14:14:08 +0530

    java

--- Revision/rev.java
+++ Revision/rev.java
@@ -1,31 +1,91 @@
+import java.util.Scanner;
+public class rev {
+// public static void main(String[] args) {
+//     Scanner sc =  new Scanner(System.in);
+//     System.out.println("Enter the num1: ");
+//     int a = sc.nextInt();
+//     System.out.println("Enter the num2: ");
+//     int b = sc.nextInt();
 
-// public class rev {
-//   public static void main(String[] args) {
-    
-//     int n = 123456;
-     
-//     while( n > 0){
-//         int lastDgit = n % 10;
-//         System.out.print(lastDgit);
-//         n = n/10;
-//     }
-//   }
+//     System.out.println(a+b);
+//     System.out.println(a-b);
+//     System.out.println(a*b);
+//     System.out.println(a/b);
 // }
 
-// import java.util.Scanner;
-
-// public class rev {
-
-//   public static void main(String[] args) {
+// public static void main(String[] args) {
 //     Scanner sc = new Scanner(System.in);
-//     System.out.print("Enter the num : ");
-//     int num = sc.nextInt();
+//     System.out.println("Enter the value of Dividend: ");
+//     int a = sc.nextInt();
+//     System.out.println("Enter the value of Divisor: ");
+//     int b = sc.nextInt();
 
-//     if(num % 2 == 0){
-//       System.out.println("Num is even");
-//     }else{
-//       System.out.println("Num is odd");
-//     }
-//   }
+//     int q = a/b;
+//     int r = a-(q*b);
+//     System.out.println(q);
+// }
+    
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("Enter the value: ");
+//     int km = sc.nextInt();
+
+//     double miles = km * 0.621;
+//     System.out.println(miles);
 // }
 
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     String frist_name = sc.next();
+//     String last_name = sc.next();
+//     int roll_no = sc.nextInt();
+//     String field_of_interest  = sc.next();
+
+//     System.out.println("Name: " +frist_name+ " "+last_name);
+//     System.out.println("Roll.no "+roll_no);
+//     System.out.println("Field of interest: "+field_of_interest);
+// }
+
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("Enter the num: ");
+//     int x = sc.nextInt();
+
+//     System.out.println("Result is: "+((((x+8) /3 ) %5 ) *5) );
+// }
+
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     for(int num = 1; num <= n ; num++){
+//     System.out.println(num);
+//     }
+// }
+
+// public static void main(String[] args) {
+//     int num = 1;
+//     while(true){
+//         if( (num%15 == 0) && (num%75 == 0) ){
+//         System.out.println(num);
+//         break;
+//         }
+//         num++;
+//     }
+// }
+
+// public static void main(String[] args) {
+    
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     int numberOfDigit = 0;
+//     int original_n = n;
+
+//     while(n>0){
+//         n=n/10;
+//         numberOfDigit++;
+//     }
+//     System.out.println("Number of Digit in " +original_n+ "=" +numberOfDigit);
+
+// }
+
+}
