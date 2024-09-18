712396a6452d4b5c54141b660a43b21a1f36365a
Tejas Jadhav <tejasnjadhav76@gmail.com>
2024-09-18 15:46:24 +0530

    java

---
+++ Classes & Objects/driver.java
@@ -0,0 +1,8 @@
+
+public class driver {
+
+    public static void main(String[] args) {
+        object myCar = new object();
+        myCar.drive();
+    }
+}
---
+++ Classes & Objects/object.java
@@ -0,0 +1,13 @@
+
+public class object {
+    float maxSpeed;
+    float currentFuelInLiters;
+    int noOfSeats;
+    
+    public void drive() {
+        System.out.println("Car is driving ");
+        currentFuelInLiters--;
+    }
+
+
+}
